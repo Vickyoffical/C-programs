6823d69ad023ce9fa883ad13c6624637f804d0d1
Vivek Mistri <126373188+Vickyoffical@users.noreply.github.com>
2024-02-22 23:37:38 +0530

    program to find factorial of given number.

---
+++ FACTORIA.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int i,fact=1,n;
+	clrscr();
+	printf("Enter the number :");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++)
+	{
+		fact=fact*i;
+	}
+	printf("Factorial of %d is %d \n",n,fact);
+	getch();
+}
